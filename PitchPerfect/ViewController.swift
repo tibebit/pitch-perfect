d9131692bb0b84d47b657495c4c882349a71e17c
tibebit <fabiotibe.bit@gmail.com>
2021-01-26 20:00:24 +0100

    Initial commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Fabio Tiberio on 18/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
+//    Called after the controller's view is loaded into memory.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains the first project in the iOS Nanodegree course from Udacity 
