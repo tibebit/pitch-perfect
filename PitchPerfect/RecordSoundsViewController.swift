e9ba4053a4df1bb68716542d5d43d184e6aa9c40
tibebit <fabiotibe.bit@gmail.com>
2021-02-03 11:07:19 +0100

    Added the code for recording sounds

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var stopRecordingButton: UIButton!
     @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder!
     
 //    Called after the controller's view is loaded into memory.
     override func viewDidLoad() {
@@ -27,11 +30,32 @@ class RecordSoundsViewController: UIViewController {
         recordingLabel.text = "Recording in Progress"
         recordButton.isEnabled = false
         stopRecordingButton.isEnabled = true
+
+//      Set-up for the recording session
+//      I retrieve the information about the path where I should store the new file
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+
+//        We setup the audio session which is basically an abstraction of the hardware that we use to record and playback sounds
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        
+//        Setup the audio recorder
+//        Note that audioRecorder.record() calls implicitly the prepareToAudio method
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
     }
+    
     @IBAction func stopRecording(_ sender: Any) {
         recordingLabel.text = "Tap to Record"
         recordButton.isEnabled = true
         stopRecordingButton.isEnabled = false
+        
     }
     
 }
