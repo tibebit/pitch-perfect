4a570e06f4420600cb984597f3e9caf8e3c6d2e6
tibebit <fabiotibe.bit@gmail.com>
2021-02-04 09:46:54 +0100

    Wired up the connection between code and buttons in PlaySoundVC

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -11,9 +11,22 @@ class PlaySoundsViewController: UIViewController {
     
     var recorderAudioURL : URL!
     
+    @IBOutlet weak var snailButton : UIButton!
+    @IBOutlet weak var rabbitButton : UIButton!
+    @IBOutlet weak var chipmunkButton : UIButton!
+    @IBOutlet weak var vaderButton : UIButton!
+    @IBOutlet weak var echoButton : UIButton!
+    @IBOutlet weak var reverbButton : UIButton!
+    @IBOutlet weak var stopButton : UIButton!
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
